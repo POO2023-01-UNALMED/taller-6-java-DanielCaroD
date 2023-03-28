311f44c71ccad02e4657bfd0104fb9e1c01c97ae
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-03-27 19:32:03 -0500

    .

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,10 +1,12 @@
 package vehiculos;
 
 public class Fabricante {
+	public int num = 0;
 	private String nombre;
 	private Pais pais;
 	public static Pais[] tPaises;
 	public static int Long = 0;
+	private static Fabricante fMas;
 	
 	
 	public Fabricante (String nombre, Pais pais) {
@@ -27,5 +29,17 @@ public class Fabricante {
 	}
 	public Pais getPais() {
 		return pais;
+	}
+	
+	public static Fabricante fabricaMayorVentas() {
+		for (int i = 0; i < Vehiculo.FLong; i++) {
+			if(i == 0) {
+				fMas = Vehiculo.tFabricantes[i];
+			}
+			else if (Vehiculo.tFabricantes[i].num > fMas.num) {
+				fMas = Vehiculo.tFabricantes[i];
+			}
+		}
+		return fMas;
 	}
 }
