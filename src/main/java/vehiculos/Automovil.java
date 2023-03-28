6ba2f9d5073855d7a51bfc9434f393449d3929dd
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-03-27 19:59:14 -0500

    .

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -6,7 +6,6 @@ public class Automovil extends Vehiculo {
 	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
 		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
 		this.puestos = puestos;
-		CantidadVehiculos ++;
 		Cautomoviles ++;
 
 	}
--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -6,7 +6,6 @@ public class Camion extends Vehiculo  {
 	public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes) {
 		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
 		this.ejes = ejes;
-		CantidadVehiculos ++;
 		Ccamiones ++;
 	}
 	
